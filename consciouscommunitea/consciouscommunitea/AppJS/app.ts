d5d38657a73a389ed93d207b83c40556d296501a
Renzo Zevalos <renzoyzevallos@gmail.com>
2016-02-16 16:45:55 -0500

    added routing

---
+++ consciouscommunitea/consciouscommunitea/AppJS/app.ts
@@ -0,0 +1,21 @@
+﻿namespace CommuniteaApp {
+    angular.module('Communitea', ['ngRoute']).config(($routeProvider: ng.route.IRouteProvider, $locationProvider: ng.ILocationProvider) => {
+
+        $routeProvider.when('/', {
+            templateUrl: '/PageTemplates/Home/home.html'
+        })
+            .otherwise('/');
+
+        $locationProvider.html5Mode(true);
+        
+        
+
+    })
+
+ 
+    
+
+
+}
+
+let communiteaApp = angular.module('Communitea');
