f85e384879bfc9d3fe7149ee2a925bcd9a523d27
Renzo Zevalos <renzoyzevallos@gmail.com>
2016-02-18 16:57:12 -0500

    commit

---
+++ consciouscommunitea/consciouscommunitea/AppJS/app.js
@@ -0,0 +1,15 @@
+var CommuniteaApp;
+(function (CommuniteaApp) {
+    angular.module('Communitea', ['ngRoute']).config(function ($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: '/PageTemplates/Home/home.html'
+        })
+            .when('/newPost', {
+            templateUrl: '/PageTemplates/NewPost/newPost.html',
+        })
+            .otherwise('/');
+        $locationProvider.html5Mode(true);
+    });
+})(CommuniteaApp || (CommuniteaApp = {}));
+var communiteaApp = angular.module('Communitea');
+//# sourceMappingURL=app.js.map
